9a61c45043dda7cf613a2a292ce938c9a7a933d3
Ido Arusi <Idoarusi1@gmail.com>
2017-01-14 20:38:07 +0200

    firstPageDone

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,29 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+export class ImagePath {
+    constructor(
+        public path: string) { }
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+    list = [
+        new ImagePath("img/img1.jpg"),
+        new ImagePath("img/img2.jpg"),
+        new ImagePath("img/img3.jpg")
+    ];
+
+    moveToCamera(){
+
+    }
   constructor(public navCtrl: NavController) {
-    
   }
 
 }
+
+
